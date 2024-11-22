bb001d0182c958f747940c213c0a70d1a7f777cc
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-11-22 02:43:12 +0200

    try 5

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+//const PORT = process.env.PORT || 8080;
+//app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
 
 module.exports = app;

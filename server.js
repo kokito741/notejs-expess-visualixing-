e52ebdaac2d848272c625c9d255990c2fc92e72b
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-11-22 03:09:50 +0200

    try 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.json());
 app.set('view engine', 'pug');
